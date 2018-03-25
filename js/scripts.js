0d284c41acc72f66d3e417891d36509c278b274c
Aneta Koppernock <akozuchowska@hotmail.com>
2018-03-25 20:26:31 +0200

    Wgranie plików

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var text = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.";
+var dinosaur = "triceratops";
+var dinosaur = dinosaur.toUpperCase();
+var text = text.replace("Velociraptor", dinosaur);
+
+console.log(text.length/2);
+console.log(text.slice(0,72));
